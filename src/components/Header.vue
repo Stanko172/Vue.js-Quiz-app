f9131f26d8cd1665f7fa007bd6b2aabf6dc2bec5
Stanko Bebek <stanko.bebek82@gmail.com>
2020-03-24 18:47:42 +0100

    implemented Bootstrap Vue

---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+<div>
+  <b-nav tabs fill>
+    <b-nav-item disabled>QuizzApp</b-nav-item>
+    <b-nav-item disabled>Counter:0/0</b-nav-item>
+  </b-nav>
+</div>
+</template>
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,19 @@
+<template>
+        <div>
+            <b-jumbotron header="BootstrapVue" lead="Bootstrap v4 Components for Vue.js 2" bg-variant="info" text-variant="white" border-variant="dark">
+
+                <template v-slot:lead>
+                    Pitanje
+                </template>
+
+                <hr class="my-4">
+
+                <p>
+                    Lista odgovora
+                </p>
+
+                <b-button variant="primary" href="#">Sljedeće</b-button>
+                <b-button variant="success" href="#">Pošalji</b-button>
+            </b-jumbotron>
+        </div>
+</template>
